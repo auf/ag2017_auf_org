0dc035d5bc75c5c02f2e2eeb93b77bd9bc9a70c9
benselme <benselme@gmail.com>
2016-09-19 16:03:12 -0400

    Inscription/dossier: ajout css champs required

--- ag/dossier_inscription/forms.py
+++ ag/dossier_inscription/forms.py
@@ -4,7 +4,7 @@ from django import forms
 
 class AdresseForm(forms.Form):
     required_css_class = 'required'
-    
+
     adresse = forms.CharField(widget=forms.Textarea, required=True)
     ville = forms.CharField(max_length=100, required=True)
     code_postal = forms.CharField(max_length=100, required=True)
