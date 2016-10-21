14248abc602edff27469e33c0c6f74e5323ff01d
Marie-Claude Pelletier <marie-claude.pelletier@auf.org>
2016-10-21 11:30:16 -0400

    remise a origine

--- manage.py
+++ manage.py
@@ -2,13 +2,6 @@
 import os
 import sys
 
-SITE_ROOT = os.path.dirname(__file__)
-
-sys.path.append(os.path.join(SITE_ROOT, '../auf-django-sdk/sdk/django18_auf/'))
-sys.path.append(os.path.join(SITE_ROOT, '../auf-django-sdk/sdk/django18_base/'))
-sys.path.append(os.path.join(SITE_ROOT, '../auf-django-sdk/sdk/django18_dev/'))
-sys.path.append(os.path.join(SITE_ROOT, '../auf-django-sdk/sdk/django18_auf_dev/'))
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ag.settings")
 
