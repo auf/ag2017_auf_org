5c133016b8ed2de56f48f2d5f8662b35e0513723
benselme <benselme@gmail.com>
2016-10-31 12:15:19 -0400

    Merge mcmaster et sabrina

--- ag/development.py
+++ ag/development.py
@@ -77,6 +77,7 @@ if 'test' in sys.argv or 'pytest_teamcity' in sys.argv or \
 
 PAYPAL_EMAIL_ADDRESS = 'benselme-facilitator@gmail.com'
 PAYPAL_URL = 'https://www.sandbox.paypal.com/cgi-bin/webscr'
+PAYPAL_PDT_TOKEN = ''
 
 SENDFILE_BACKEND="sendfile.backends.development"
 #LOGGING = {
