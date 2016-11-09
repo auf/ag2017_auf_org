0320d0f2234245ed006f7b26c690f909091232d4
benselme <benselme@gmail.com>
2016-11-09 13:09:26 -0500

    Mise en place dump données cms

--- ag/dump_cms_data.sh
+++
@@ -1,1 +0,0 @@
-python manage.py dumpdata cms djangocms_text_ckeditor filer easy_thumbnails djangocms_link cmsplugin_filer_file cmsplugin_filer_folder cmsplugin_filer_image cmsplugin_filer_utils menus tinymce auth.User
