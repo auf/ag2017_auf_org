c54c48924cb247b65137a2585b5ee398e7027850
benselme <benselme@gmail.com>
2016-11-09 16:23:59 -0500

    Charger cms_data.json et auth_user.json dans init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -2,4 +2,6 @@ mysql -u root -e "DROP SCHEMA IF EXISTS $1; CREATE SCHEMA $1 DEFAULT CHARACTER S
 python ./manage.py migrate
 mysql -u root $1 < ag/reference/load_from_datamaster.sql
 python manage.py loaddata was_initial_data.json
+python manage.py loaddata cms_data.json
+python manage.py loaddata ./auth_user.json
 python manage.py generer_invitations_mandates
