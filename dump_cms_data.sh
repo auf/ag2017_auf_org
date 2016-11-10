b774445b6e9bbee1c9a184820e01be2935484e3e
benselme <benselme@gmail.com>
2016-11-10 16:01:18 -0500

    Inclure app 'outil' dans dump cms data

--- dump_cms_data.sh
+++ dump_cms_data.sh
@@ -1,2 +1,2 @@
-python manage.py dumpdata cms djangocms_text_ckeditor filer easy_thumbnails djangocms_link cmsplugin_filer_file cmsplugin_filer_folder cmsplugin_filer_image cmsplugin_filer_utils menus tinymce>ag/core/fixtures/cms_data.json
+python manage.py dumpdata cms outil djangocms_text_ckeditor filer easy_thumbnails djangocms_link cmsplugin_filer_file cmsplugin_filer_folder cmsplugin_filer_image cmsplugin_filer_utils menus tinymce>ag/core/fixtures/cms_data.json
 python manage.py dumpdata auth.User>auth_user.json
