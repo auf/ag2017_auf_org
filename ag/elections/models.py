11376ca7ba6f7e3159aee4b3b5c340a508f1dfda
benselme <benselme@gmail.com>
2017-03-02 15:02:14 -0500

    Modèle et fixtures élections

---
+++ ag/elections/models.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+
+from django.db.models import IntegerField
+
+from ag.core.models import TableReference
+
+
+class Election(TableReference):
+    nb_sieges_global = IntegerField(u"Global", blank=True, null=True)
+    nb_sieges_afrique = IntegerField(u"Afrique", blank=True, null=True)
+    nb_sieges_ameriques = IntegerField(u"Amériques", blank=True, null=True)
+    nb_sieges_asie_pacifique = IntegerField(u"Asie-Pacifique",
+                                            blank=True, null=True)
+    nb_sieges_europe_ouest = IntegerField(u"Europe de l'ouest",
+                                          blank=True, null=True)
+    nb_sieges_europe_est = IntegerField(u"Europe centrale et orientale",
+                                        blank=True, null=True)
+    nb_sieges_maghreb = IntegerField(u"Maghreb", blank=True, null=True)
+    nb_sieges_moyen_orient = IntegerField(u"Moyen-Orient", blank=True,
+                                         null=True)
